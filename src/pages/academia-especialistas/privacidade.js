84acb8e71f148a462eb6e36472e70cb7d810d839
Matheus Reis <reismatheus97@gmail.com>
2021-10-08 11:11:38 -0300

    add privacy terms for academia-especialistas

---
+++ src/pages/academia-especialistas/privacidade.js
@@ -0,0 +1,187 @@
+import React from 'react';
+import '../../assets/privacidade.css'
+
+export default () => (
+    <section className="pt-20 md:pt-40">
+      <h1>
+        POL&Iacute;TICA DE PRIVACIDADE - <br/>
+        ACADEMIA DE ESPECIALISTAS
+      </h1>
+      <div class="c12 main-wrapper"><a id="id.gjdgxs"></a>
+        <p class="c1 c3"><span class="c8 c7"></span></p>
+        <p class="c1"><span class="c7">SE&Ccedil;&Atilde;O 01 - INFORMA&Ccedil;&Otilde;ES GERAIS</span></p>
+        <p class="c1 c3"><span class="c0"></span></p>
+        <p class="c1"><span class="c6">A presente Pol&iacute;tica de Privacidade cont&eacute;m informa&ccedil;&otilde;es sobre
+          coleta, uso, armazenamento, tratamento e prote&ccedil;&atilde;o dos dados pessoais dos usu&aacute;rios do
+          aplicativo </span><span class="c7">ACADEMIA DE ESPECIALISTAS</span><span class="c0">, com a finalidade de
+            demonstrar absoluta transpar&ecirc;ncia quanto ao assunto e esclarecer a todos interessados sobre os tipos de
+            dados que s&atilde;o coletados, os motivos da coleta e a forma como os usu&aacute;rios podem gerenciar ou excluir
+            as suas informa&ccedil;&otilde;es pessoais.</span></p>
+        <p class="c1 c3"><span class="c0"></span></p>
+        <p class="c1"><span class="c6">Esta Pol&iacute;tica de Privacidade aplica-se a todos os usu&aacute;rios do aplicativo
+        </span><span class="c7">ACADEMIA DE ESPECIALISTAS</span><span class="c6">&nbsp;e integra os Termos e
+          Condi&ccedil;&otilde;es Gerais de Uso, devidamente inscrita no </span><span class="c7">CNPJ sob o n&ordm;
+            38.212.806/0001-16</span><span class="c0">, situado em Avenida Armando Lombardi 1000 &ndash; bloco 01 &ndash; sala
+              219, doravante nominada PAD MED ENSINO EM MEDICINA LTDA.</span></p>
+        <p class="c1 c3"><span class="c0"></span></p>
+        <p class="c1"><span class="c6">O presente documento foi elaborado em conformidade com a Lei Geral de
+          Prote&ccedil;&atilde;o de Dados Pessoais (Lei </span><span class="c4"><a class="c2"
+            href="https://www.google.com/url?q=https://www.jusbrasil.com.br/legislacao/612902269/lei-13709-18&amp;sa=D&amp;source=editors&amp;ust=1633674043928000&amp;usg=AOvVaw0dqLQUexYGE3ookihyg5PZ">13.709</a></span><span
+              class="c6">/18), o </span><span class="c4"><a class="c2"
+                href="https://www.google.com/url?q=https://www.jusbrasil.com.br/legislacao/117197216/lei-12965-14&amp;sa=D&amp;source=editors&amp;ust=1633674043928000&amp;usg=AOvVaw17R4NbofqtVV7DBqmBx9DK">Marco
+                Civil da Internet</a></span><span class="c6">&nbsp;(Lei </span><span class="c4"><a class="c2"
+                  href="https://www.google.com/url?q=https://www.jusbrasil.com.br/legislacao/117197216/lei-12965-14&amp;sa=D&amp;source=editors&amp;ust=1633674043929000&amp;usg=AOvVaw29nt8WJsQYRHizs6FFXvdV">12.965</a></span><span
+                    class="c0">/14) (e o Regulamento da UE n. 2016/6790). Ainda, o documento poder&aacute; ser atualizado em
+            decorr&ecirc;ncia de eventual atualiza&ccedil;&atilde;o normativa, raz&atilde;o pela qual se convida o
+            usu&aacute;rio a consultar periodicamente esta se&ccedil;&atilde;o.</span></p>
+        <p class="c1 c3"><span class="c0"></span></p>
+        <p class="c1"><span class="c7">SE&Ccedil;&Atilde;O 02 - COMO RECOLHEMOS OS DADOS PESSOAIS DO USU&Aacute;RIO?</span>
+        </p>
+        <p class="c1"><span class="c0">Os dados pessoais do usu&aacute;rio s&atilde;o recolhidos pela plataforma da seguinte
+          forma:</span></p>
+        <p class="c1 c3"><span class="c0"></span></p>
+        <ul class="c10 lst-kix_list_7-0 start">
+          <li class="c1 c11 li-bullet-0"><span class="c0">Ao adquirir o curso ACADEMIA DE ESPECIALISTAS na plataforma Hotmart,
+            a plataforma do aplicativo se comunica atrav&eacute;s de um webhook. Webhook &eacute; uma forma de recebimento
+            de informa&ccedil;&otilde;es. &Eacute; uma maneira pr&aacute;tica para um app ou sistema fornecer outras
+            aplica&ccedil;&otilde;es com informa&ccedil;&otilde;es em tempo real.</span></li>
+        </ul>
+        <p class="c1 c3"><span class="c8 c7"></span></p>
+        <p class="c1"><span class="c7 c8">SE&Ccedil;&Atilde;O 03 - QUAIS DADOS PESSOAIS RECOLHEMOS SOBRE O USU&Aacute;RIO E
+          VISITANTE?</span></p>
+        <p class="c1"><span class="c0">Os seguintes dados pessoais do usu&aacute;rio s&atilde;o recolhidos:</span></p>
+        <p class="c1 c3"><span class="c0"></span></p>
+        <ul class="c10 lst-kix_list_7-0">
+          <li class="c1 c11 li-bullet-0"><span class="c0">No momento da aquisi&ccedil;&atilde;o do curso, quatro
+            informa&ccedil;&otilde;es s&atilde;o repassadas pelo Hotmart que possibilitam o usu&aacute;rio a se logar. ao
+            app. S&atilde;o elas: nome, e-mail, senha e o nome do curso comprado. Essa &uacute;ltima
+            informa&ccedil;&atilde;o determina o n&iacute;vel de acesso ao conte&uacute;do do aplicativo. J&aacute; dentro
+            do aplicativo, mais duas informa&ccedil;&otilde;es s&atilde;o coletadas: o nome da Unidade da
+            Federa&ccedil;&atilde;o e o nome do Servi&ccedil;o de Resid&ecirc;ncia que o usu&aacute;rio pertence.</span>
+          </li>
+        </ul>
+        <p class="c1 c3"><span class="c0"></span></p>
+        <p class="c1"><span class="c7">SE&Ccedil;&Atilde;O 04 - PARA QUE FINALIDADES UTILIZAMOS OS DADOS PESSOAIS DO
+          USU&Aacute;RIO E VISITANTE?</span></p>
+        <p class="c1"><span class="c0">Os dados pessoais do usu&aacute;rio coletados e armazenados pela plataforma tem por
+          finalidade:</span></p>
+        <p class="c1 c3"><span class="c0"></span></p>
+        <ul class="c10 lst-kix_list_8-0 start">
+          <li class="c1 c9 li-bullet-1"><span class="c0">Bem-estar do usu&aacute;rio e visitante: aprimorar o produto e/ou
+            servi&ccedil;o oferecido, facilitar, agilizar e cumprir os compromissos estabelecidos entre o usu&aacute;rio e a
+            empresa, melhorar a experi&ecirc;ncia dos usu&aacute;rios e fornecer funcionalidades espec&iacute;ficas a
+            depender das caracter&iacute;sticas b&aacute;sicas do usu&aacute;rio.</span></li>
+          <li class="c1 c9 li-bullet-2"><span class="c0">Melhorias da plataforma: compreender como o usu&aacute;rio utiliza os
+            servi&ccedil;os da plataforma, para ajudar no desenvolvimento de neg&oacute;cios e t&eacute;cnicas.</span></li>
+          <li class="c1 c9 li-bullet-2"><span class="c0">Comercial: os dados s&atilde;o usados para personalizar o
+            conte&uacute;do oferecido e gerar subs&iacute;dio &agrave; plataforma para a melhora da qualidade no
+            funcionamento dos servi&ccedil;os.</span></li>
+          <li class="c1 c9 li-bullet-1"><span class="c0">Previs&atilde;o do perfil do usu&aacute;rio: tratamento automatizados
+            de dados pessoais para avaliar o uso na plataforma.</span></li>
+          <li class="c1 c9 li-bullet-2"><span class="c0">Dados de cadastro: para permitir o acesso do usu&aacute;rio a
+            determinados conte&uacute;dos da plataforma, exclusivo para usu&aacute;rios cadastrados</span></li>
+          <li class="c1 c9 li-bullet-2"><span class="c0">Dados de contrato: conferir &agrave;s partes seguran&ccedil;a
+            jur&iacute;dica e facilitar a conclus&atilde;o do neg&oacute;cio.</span></li>
+          <li class="c1 c9 li-bullet-1"><span class="c0">Outras</span></li>
+        </ul>
+        <p class="c1 c3"><span class="c0"></span></p>
+        <p class="c1"><span class="c0">O tratamento de dados pessoais para finalidades n&atilde;o previstas nesta
+          Pol&iacute;tica de Privacidade somente ocorrer&aacute; mediante comunica&ccedil;&atilde;o pr&eacute;via ao
+          usu&aacute;rio, de modo que os direitos e obriga&ccedil;&otilde;es aqui previstos permanecem
+          aplic&aacute;veis.</span></p>
+        <p class="c1 c3"><span class="c8 c7"></span></p>
+        <p class="c1"><span class="c7">SE&Ccedil;&Atilde;O 05 - POR QUANTO TEMPO OS DADOS PESSOAIS FICAM ARMAZENADOS?</span>
+        </p>
+        <p class="c1"><span class="c6">Os dados pessoais do usu&aacute;rio s&atilde;o armazenados pela plataforma durante o
+          per&iacute;odo necess&aacute;rio para a presta&ccedil;&atilde;o do servi&ccedil;o ou o cumprimento das finalidades
+          previstas no presente documento, conforme o disposto no inciso </span><span class="c4"><a class="c2"
+            href="https://www.google.com/url?q=https://www.jusbrasil.com.br/topicos/200399061/inciso-i-do-artigo-15-da-lei-n-13709-de-14-de-agosto-de-2018&amp;sa=D&amp;source=editors&amp;ust=1633674043932000&amp;usg=AOvVaw04dsm-8alKyApwXR6MCPki">I</a></span><span
+              class="c6">&nbsp;do artigo </span><span class="c4"><a class="c2"
+                href="https://www.google.com/url?q=https://www.jusbrasil.com.br/topicos/200399064/artigo-15-da-lei-n-13709-de-14-de-agosto-de-2018&amp;sa=D&amp;source=editors&amp;ust=1633674043932000&amp;usg=AOvVaw2KgxXlt9tW7l4u6kNEiTmd">15</a></span><span
+                  class="c6">&nbsp;da Lei </span><span class="c4"><a class="c2"
+                    href="https://www.google.com/url?q=https://www.jusbrasil.com.br/legislacao/612902269/lei-13709-18&amp;sa=D&amp;source=editors&amp;ust=1633674043933000&amp;usg=AOvVaw0GyhuYM5ETN3AhM-dAELr-">13.709</a></span><span
+                      class="c0">/18.</span></p>
+        <p class="c1 c3"><span class="c0"></span></p>
+        <p class="c1"><span class="c0">Os dados podem ser removidos ou anonimizados a pedido do usu&aacute;rio, excetuando os
+          casos em que a lei oferecer outro tratamento. No caso de solicita&ccedil;&atilde;o de remo&ccedil;&atilde;o dos
+          dados impossibilitar&aacute; a utiliza&ccedil;&atilde;o do aplicativo. Nenhum reembolso ser&aacute; devido, uma
+          vez que o app &eacute; uma disponibilidade gratuita aos usu&aacute;rios que comprarem o curso ACADEMIA DE
+          ESPECIALISTAS.</span></p>
+        <p class="c1 c3"><span class="c0"></span></p>
+        <p class="c1"><span class="c0">Ainda, os dados pessoais dos usu&aacute;rios apenas podem ser conservados ap&oacute;s o
+          t&eacute;rmino de seu tratamento nas seguintes hip&oacute;teses previstas no artigo 16 da referida lei:</span></p>
+        <p class="c1"><span class="c0">I - cumprimento de obriga&ccedil;&atilde;o legal ou regulat&oacute;ria pelo
+          controlador;</span></p>
+        <p class="c1"><span class="c0">II - estudo por &oacute;rg&atilde;o de pesquisa, garantida, sempre que poss&iacute;vel,
+          a anonimiza&ccedil;&atilde;o dos dados pessoais;</span></p>
+        <p class="c1"><span class="c0">III - transfer&ecirc;ncia a terceiro, desde que respeitados os requisitos de tratamento
+          de dados dispostos nesta Lei;</span></p>
+        <p class="c1"><span class="c0">IV - uso exclusivo do controlador, vedado seu acesso por terceiro, e desde que
+          anonimizados os dados.</span></p>
+        <p class="c1 c3"><span class="c8 c7"></span></p>
+        <p class="c1"><span class="c7">SE&Ccedil;&Atilde;O 06 - SEGURAN&Ccedil;A DOS DADOS PESSOAIS ARMAZENADOS</span></p>
+        <p class="c1"><span class="c0">A plataforma se compromete a aplicar as medidas t&eacute;cnicas e organizativas aptas a
+          proteger os dados pessoais de acessos n&atilde;o autorizados e de situa&ccedil;&otilde;es de
+          destrui&ccedil;&atilde;o, perda, altera&ccedil;&atilde;o, comunica&ccedil;&atilde;o ou difus&atilde;o de tais
+          dados.</span></p>
+        <p class="c1 c3"><span class="c0"></span></p>
+        <p class="c1"><span class="c0">A plataforma se exime de responsabilidade por culpa exclusiva de terceiros, como em
+          caso de ataque de hackers ou crackers, ou culpa exclusiva do usu&aacute;rio, como no caso em que ele mesmo
+          transfere seus dados a terceiros. O site se compromete a comunicar o usu&aacute;rio em caso de alguma
+          viola&ccedil;&atilde;o de seguran&ccedil;a dos seus dados pessoais.</span></p>
+        <p class="c1 c3"><span class="c0"></span></p>
+        <p class="c1"><span class="c0">Os dados pessoais armazenados s&atilde;o tratados com confidencialidade, dentro dos
+          limites legais. No entanto, podemos divulgar suas informa&ccedil;&otilde;es pessoais caso sejamos obrigados pela
+          lei para faz&ecirc;-lo ou se voc&ecirc; violar nossos Termos de Servi&ccedil;o.</span></p>
+        <p class="c1 c3"><span class="c8 c7"></span></p>
+        <p class="c1"><span class="c7">SE&Ccedil;&Atilde;O 07 - COMPARTILHAMENTO DOS DADOS</span></p>
+        <p class="c1"><span class="c0">Os dados do perfil do usu&aacute;rio n&atilde;o s&atilde;o compartilhados.</span></p>
+        <p class="c1 c3"><span class="c8 c7"></span></p>
+        <p class="c1"><span class="c7">SE&Ccedil;&Atilde;O 08 - OS DADOS PESSOAIS ARMAZENADOS SER&Atilde;O TRANSFERIDOS A
+          TERCEIROS?</span></p>
+        <p class="c1"><span class="c0">Os dados pessoais n&atilde;o ser&atilde;o compartilhados com Terceiros.</span></p>
+        <p class="c1 c3"><span class="c8 c7"></span></p>
+        <p class="c1"><span class="c7">SE&Ccedil;&Atilde;O 09 &ndash; COOKIES OU DADOS DE NAVEGA&Ccedil;&Atilde;O</span></p>
+        <p class="c1"><span class="c0">O aplicativo n&atilde;o utiliza cookies e n&atilde;o coleta dados de
+          navega&ccedil;&atilde;o</span></p>
+        <p class="c1 c3"><span class="c8 c7"></span></p>
+        <p class="c1"><span class="c7">SE&Ccedil;&Atilde;O 10 - CONSENTIMENTO</span></p>
+        <p class="c1"><span class="c0">Ao utilizar os servi&ccedil;os e fornecer as informa&ccedil;&otilde;es pessoais na
+          plataforma, o usu&aacute;rio est&aacute; consentindo com a presente Pol&iacute;tica de Privacidade.</span></p>
+        <p class="c1 c3"><span class="c0"></span></p>
+        <p class="c1"><span class="c0">O usu&aacute;rio, ao cadastrar-se, manifesta conhecer e pode exercitar seus direitos de
+          cancelar seu cadastro, acessar e atualizar seus dados pessoais e garante a veracidade das
+          informa&ccedil;&otilde;es por ele disponibilizadas.</span></p>
+        <p class="c1 c3"><span class="c0"></span></p>
+        <p class="c1"><span class="c0">O usu&aacute;rio tem direito de retirar o seu consentimento a qualquer tempo, para
+          tanto deve entrar em contato atrav&eacute;s do perfil da empresa no Instagram.</span></p>
+        <p class="c1 c3"><span class="c0"></span></p>
+        <p class="c1"><span class="c0">A retirada do consentimento implicar&aacute;, necessariamente, a perda de acesso ao
+          aplicativo e a plataforma.</span></p>
+        <p class="c1 c3"><span class="c0"></span></p>
+        <p class="c1"><span class="c7">SE&Ccedil;&Atilde;O 11 - ALTERA&Ccedil;&Otilde;ES PARA ESSA POL&Iacute;TICA DE
+          PRIVACIDADE</span></p>
+        <p class="c1"><span class="c0">Reservamos o direito de modificar essa Pol&iacute;tica de Privacidade a qualquer
+          momento, ent&atilde;o, &eacute; recomend&aacute;vel que o usu&aacute;rio e visitante revise-a com
+          frequ&ecirc;ncia.</span></p>
+        <p class="c1 c3"><span class="c0"></span></p>
+        <p class="c1"><span class="c0">As altera&ccedil;&otilde;es e esclarecimentos v&atilde;o surtir efeito imediatamente
+          ap&oacute;s sua publica&ccedil;&atilde;o na plataforma. Quando realizadas altera&ccedil;&otilde;es os
+          usu&aacute;rios ser&atilde;o notificados. Ao utilizar o servi&ccedil;o ou fornecer informa&ccedil;&otilde;es
+          pessoais ap&oacute;s eventuais modifica&ccedil;&otilde;es, o usu&aacute;rio e visitante demonstra sua
+          concord&acirc;ncia com as novas normas.</span></p>
+        <p class="c1 c3"><span class="c0"></span></p>
+        <p class="c1"><span class="c0">Diante da fus&atilde;o ou venda da plataforma &agrave; outra empresa os dados dos
+          usu&aacute;rios podem ser transferidos para os novos propriet&aacute;rios para que a perman&ecirc;ncia dos
+          servi&ccedil;os oferecidos.</span></p>
+        <p class="c1 c3"><span class="c8 c7"></span></p>
+        <p class="c1"><span class="c7">SE&Ccedil;&Atilde;O 12 &ndash; JURISDI&Ccedil;&Atilde;O PARA RESOLU&Ccedil;&Atilde;O DE
+          CONFLITOS</span></p>
+        <p class="c1"><span class="c0">Para a solu&ccedil;&atilde;o de controv&eacute;rsias decorrentes do presente
+          instrumento ser&aacute; aplicado integralmente o Direito brasileiro.</span></p>
+        <p class="c1 c3"><span class="c0"></span></p>
+        <p class="c1"><span class="c0">Os eventuais lit&iacute;gios dever&atilde;o ser apresentados no foro da comarca em que
+          se encontra a sede da empresa, ou seja na capital do Estado do Rio de Janeiro.</span></p>
+      </div>
+    </section>
+);
